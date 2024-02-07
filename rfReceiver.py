93875b3ea63207a7a312f54a3efef590a32f7fd6
yxxyz <outtaleovaldez@yahoo.com>
2024-02-07 15:49:22 +0800

    edited the file a bit

--- rfReceiver.py
+++ rfReceiver.py
@@ -26,60 +26,6 @@ cfg = {"spi": spi, "miso": 4, "mosi": 7, "sck": 6, "csn": 5, "ce": 8}
 # 0xf0f0f0f0e1, 0xf0f0f0f0d2
 pipes = (b"\xe1\xf0\xf0\xf0\xf0", b"\xd2\xf0\xf0\xf0\xf0")
 
-
-def initiator():
-    csn = Pin(cfg["csn"], mode=Pin.OUT, value=1)
-    ce = Pin(cfg["ce"], mode=Pin.OUT, value=0)
-    spi = cfg["spi"]
-    nrf = NRF24L01(spi, csn, ce, payload_size=8)
-
-    nrf.open_tx_pipe(pipes[0])
-    nrf.open_rx_pipe(1, pipes[1])
-    nrf.start_listening()
-
-    num_needed = 16
-    num_successes = 0
-    num_failures = 0
-    led_state = 0
-
-    print("NRF24L01 initiator mode, sending %d packets..." % num_needed)
-
-    while num_successes < num_needed and num_failures < num_needed:
-        # stop listening and send packet
-        nrf.stop_listening()
-        mssg = "cibai"
-        print("sending:", mssg)
-        try:
-            nrf.send(mssg.encode('utf-8'))
-        except OSError:
-            pass
-
-        # start listening again
-        nrf.start_listening()
-
-        # wait for response, with 250ms timeout
-        start_time = utime.ticks_ms()
-        timeout = False
-        while not nrf.any() and not timeout:
-            if utime.ticks_diff(utime.ticks_ms(), start_time) > 250:
-                timeout = True
-
-        if timeout:
-            print("failed, response timed out")
-            num_failures += 1
-
-        else:
-            # recv packet
-            pingedMssg = nrf.recv().decode('utf-8')
-
-            # print response and round-trip delay
-            num_successes += 1
-
-        # delay then loop
-        utime.sleep_ms(250)
-
-    print("initiator finished sending; successes=%d, failures=%d" % (num_successes, num_failures))
-
 def responder():
     csn = Pin(cfg["csn"], mode=Pin.OUT, value=1)
     ce = Pin(cfg["ce"], mode=Pin.OUT, value=0)
@@ -112,8 +58,10 @@ def responder():
             try:
                 pingMssg = "Got it!"
                 nrf.send(pingMssg.encode('utf-8'))
+                print("sent response: ", pingMssg)
+
             except OSError:
                 pass
             
-            print("sent response")
+            # Start listening to RF messages again
             nrf.start_listening()
