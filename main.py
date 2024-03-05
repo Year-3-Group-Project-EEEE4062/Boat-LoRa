4af1c0cd8b9c2adae80f782020ccee0caf00c3a7
Arfan <arfandnl@gmail.com>
2024-03-05 13:21:10 +0800

    Direct USB communication
    - Only added writing from Pico to RPi 4

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from machine import Pin, I2C
+from machine import Pin
+import select
+import sys
 import time
+import ubinascii
 
 import boatLoRa
+
 
 ##################################################################
 ##################################################################
 ## Callback when data received through LoRa
 def receivedLoRa(payload):
-    print("Received LoRa: ", payload.message)
+    # Convert bytearray to base64-encoded string
+    base64_string = ubinascii.b2a_base64(payload.message).decode('utf-8').strip()
+
+    # Convert bytearray to byte strings
+    sys.stdout.write(base64_string+'\n')
+
+    # let the RPi4 know new data
+    TX_INT.toggle()
 
 ##################################################################
 ##################################################################
 ## Callback when data received from RPi 4 (main boat brain)
-def receivedI2C(pin):
+def receivedFromBrain(pin):
     print("Received data from Brain!!")
 
 ##################################################################
 ##################################################################
 ## Initialization
 # loraModule_TX = boatLoRa.boatLoRa_TX()
-# print("Boat LoRa TX Initialized!!")
+# print("Pico W LoRa TX Initialized!!")
 
 loraModule_RX = boatLoRa.boatLoRa_RX()
-print("Boat LoRa RX Initialized!!")
+print("Pico W LoRa RX Initialized!!")
 
 # Setup on board LED to let user know also if BLE connected or not 
 led = Pin("LED", Pin.OUT)
 led.off()
 
-# Setup I2C to exchange data with brain
-sdaPin = Pin(14)
-sclPin = Pin(15)
-i2c=machine.I2C(0,sda=sdaPIN, scl=sclPIN, freq=400000)
-TX_INT = Pin(12, Pin.OUT) # White wire
-RX_INT = Pin(13, Pin.OUT) # Blue wire
+# Set up GPIO Interrupt pin upon change in state of this pin
+TX_INT = Pin(15, Pin.OUT) # White wire
+RX_INT = Pin(14, Pin.IN) # Blue wire
+RX_INT.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=receivedFromBrain)
+print("Pico W Serial INT Initialized")
 
 ##################################################################
 ## main operation
@@ -42,13 +52,10 @@ RX_INT = Pin(13, Pin.OUT) # Blue wire
 # loraModule_TX.loraSenderTest()
 loraModule_RX.loraRX(receivedLoRa)
 
-# Set up GPIO Interrupt pin upon change in state of this pin
-RX_INT.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=receivedI2C)
+led.on() # Indicate everything initialized
 
-time.sleep(10)
-
-while True:
-    TX_INT.toggle()
-    time.sleep(5)
-    print("Toggled!!")
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
     pass
